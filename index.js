ea7269e76b8bc21d0f2aa3f16984fe9e362d2e43
+ <+>
2022-07-28 11:22:30 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 function fetchBooks() {
+  fetch("https://anapioficeandfire.com/api/books")
+  return fetch("https://anapioficeandfire.com/api/books")
+  .then(res => res.json())
+  .then(json =>renderBooks(json))
+  
   // To pass the tests, don't forget to return your fetch!
   
 }
